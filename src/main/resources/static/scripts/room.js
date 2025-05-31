5eaa7a7e6802028e56fe58448f0bc0027f0399bf
Aritra Mondal <aritramondal855@gmail.com>
2025-05-31 20:09:43 +0530

    rooms now displayed

--- src/main/resources/static/scripts/room.js
+++ src/main/resources/static/scripts/room.js
@@ -1,9 +1,20 @@
+const roomsContainer = document.getElementById("rooms");
+
 async function getRooms() {
     const url = "/api/rooms"
     try {
         const res = await fetch(url);
         const json = await res.json();
         console.log(json);
+        json.forEach(room => {
+            const roomDiv = roomsContainer
+                .appendChild(document.createElement("div"));
+            roomDiv
+                .innerHTML = `Room number: ${room.room_number}<br />
+                    Room type: ${room.roomType}<br />
+                    ${room.occupied ? "Not available" : "Available"}`;
+            roomDiv.className = "room"
+        })
     } catch (e) {
         console.error(e.message);
     }
